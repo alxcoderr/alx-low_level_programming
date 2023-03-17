9541bb78033f65e86a5e392da12ca85d8394e107
Ridwan <bigboyhammed@gmail.com>
2023-03-16 21:54:14 -0700

    v

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,8 @@
-#include <stdio.h>
-/**
-* main - entry point where the program start to execute.
-*
-* return - return the function and stop the program */
+#include <unistd.h>
 
-int main(void)
-{
-	printf("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
-	return(0);
+int main(void) {
+  char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  write(STDERR_FILENO, message, sizeof(message) - 1);
+  return 1;
 }
+
